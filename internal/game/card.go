efe12730a977e7c949d20bcebc8455d4cc03c496
ameer005 <ak2260650@gmail.com>
2026-02-15 04:48:11 +0530

    feat(card): add basic logic

--- internal/game/card.go
+++ internal/game/card.go
@@ -1,8 +1,60 @@
 package game
 
-type Card struct{}
-
-func NewCard() *Card {
-	return &Card{}
+type Card struct {
+	Name              string
+	price             int
+	quantity          int
+	availableQuantity int
 }
 
+func NewCard(name string, price, quantity, availabelQuantity int) *Card {
+	return &Card{
+		Name:              name,
+		price:             price,
+		quantity:          quantity,
+		availableQuantity: availabelQuantity,
+	}
+}
+
+// updaters
+func (c *Card) UpdatePrice(price int) bool {
+	c.price = price
+	return true
+}
+
+func (c *Card) DecreaseAvailableQuantity(amount int) bool {
+	newQty := c.availableQuantity - amount
+
+	if newQty < 0 {
+		return false
+	}
+
+	c.availableQuantity = newQty
+
+	return true
+}
+
+func (c *Card) IncreaseAvailableQuantity(amount int) bool {
+	newQty := c.availableQuantity + amount
+
+	if newQty > c.quantity {
+		return false
+	}
+
+	c.availableQuantity = newQty
+
+	return true
+}
+
+// getters
+func (c *Card) Price() int {
+	return c.price
+}
+
+func (c *Card) Quantity() int {
+	return c.quantity
+}
+
+func (c *Card) AvailaleQuantity() int {
+	return c.availableQuantity
+}
